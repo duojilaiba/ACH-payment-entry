52104357f406d033f6cae2593b907f5e59567bf0
wyt111 <1003934654@qq.com>
2022-07-26 16:23:06 +0800

    选择支付方式this指向

--- src/views/buyCurrency/payments/creditCard/payForm-cardInfo.vue
+++ src/views/buyCurrency/payments/creditCard/payForm-cardInfo.vue
@@ -357,20 +357,20 @@ export default {
       queryParams.source = 0;
       let _this = this
       this.$axios.post(this.$api.post_saveCardInfo,queryParams,'').then(res=>{
-        this.request_loading = false;
+        _this.request_loading = false;
         if(res && res.returnCode === '0000'){
           queryParams.cardNumber = queryParams.cardNumber.replace(/ /g,'');
           queryParams.userCardId = res.data.userCardId;
-          this.$store.state.buyRouterParams.userCardId = res.data.userCardId;
-          this.$store.state.sellRouterParams.fullName = AES_Decrypt(res.data.firstname) + ' '+ AES_Decrypt(res.data.lastname)
-      this.$store.state.sellRouterParams.fullName = AES_Encrypt(this.$store.state.sellRouterParams.fullName)
+          _this.$store.state.buyRouterParams.userCardId = res.data.userCardId;
+          _this.$store.state.sellRouterParams.fullName = AES_Decrypt(res.data.firstname) + ' '+ AES_Decrypt(res.data.lastname)
+          _this.$store.state.sellRouterParams.fullName = AES_Encrypt(_this.$store.state.sellRouterParams.fullName)
           //是否验证过baseId
-          _this.$axios.post(this.$api.post_getKycThrough).then(_res=>{
+          _this.$axios.post(this.$api.post_getKycThrough,'','').then(_res=>{
               if(_res && _res.returnCode === '0000'){
-                _this.$store.state.WhichPage = `/creditCardConfig?submitForm=${JSON.stringify(queryParams)}&merchant_orderNo=${this.$route.query.merchant_orderNo}`
-                this.$router.push('/kycVerification')
+                _this.$store.state.WhichPage = `/creditCardConfig?submitForm=${JSON.stringify(queryParams)}&merchant_orderNo=${_this.$route.query.merchant_orderNo}`
+                _this.$router.push('/kycVerification')
               }else{
-                _this.$router.replace(`/creditCardConfig?submitForm=${JSON.stringify(queryParams)}&merchant_orderNo=${this.$route.query.merchant_orderNo}`);
+                _this.$router.replace(`/creditCardConfig?submitForm=${JSON.stringify(queryParams)}&merchant_orderNo=${_this.$route.query.merchant_orderNo}`);
               }
           })
           // if(this.$store.state.buyRouterParams.kyc === true){
