2c672d07d109ddc4b68721360b5ae1777e09b5c9
QxsDong <102142250+QxsDong@users.noreply.github.com>
2022-07-28 20:15:56 +0800

    修改了卖币状态

--- src/views/orderState/children/sendCrypto.vue
+++ src/views/orderState/children/sendCrypto.vue
@@ -29,7 +29,7 @@
     </div>
     <div class="sendCrypto_qrcode">
       <p :style="{color:transferredShow?'#063376 !important':''}">{{ $t('nav.Sellorder_transfer') }} {{orderStateData.cryptoCurrency}} {{ $t('nav.Sellorder_within') }} <span>{{ turnMinute(orderStateData.expirationTime) }}</span></p>
-      <div ref="qrCodeUrl" class="qrCodeUrl" style="width:1.4rem;height:1.4rem;"></div>
+      <div ref="qrCodeUrl" class="qrCodeUrl"></div>
     </div>
     <div class="sendCrypto_content" style=" align-items: center;">
       <div class="title">{{ $t('nav.Sellorder_Address') }} ({{orderStateData.cryptocurrency}})</div>
@@ -445,7 +445,7 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding-bottom: .3rem;
+    padding-bottom: .2rem;
     font-family: SFProDisplayRegular;
     p{
       font-size: .13rem;
