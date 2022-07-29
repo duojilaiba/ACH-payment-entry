3e53d0699d8f32a13ad0e211686e3fc7190de442
QxsDong <102142250+QxsDong@users.noreply.github.com>
2022-07-29 11:16:18 +0800

    修复了登陆跳转菜单栏还是打开问题

--- src/views/emailCode/index.vue
+++ src/views/emailCode/index.vue
@@ -85,6 +85,7 @@ export default {
     }
   },
   activated(){
+    this.$parent.routerViewState = true
     this.login_loading= true
     this.code = "";
     this.timeDown = 60;
@@ -115,6 +116,7 @@ export default {
     this.timeVal = null;
   },
   mounted(){
+    this.$parent.routerViewState = true
    setTimeout(()=>{
      if(localStorage.getItem('login_email')){
         this.email = AES_Decrypt(localStorage.getItem('login_email'))
